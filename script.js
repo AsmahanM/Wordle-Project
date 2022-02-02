88acc22bdc236804ed25437a33d99f7061f81c01
Asmahan <asmahanhmohamed@gmail.com>
2022-02-02 21:15:01 +0000

    Created tiles. Each tile has an ID.

--- script.js
+++ script.js
@@ -14,13 +14,16 @@ window.onload = function() {
 function initialize() {//will create tiles and event listeners for game
 
 //THE ACTUAL BOARD:
-
+//the following for loop should help us create 30 tile elements with  escalating ID numbers representing the current row (attempt) and column (current letter/position in the word)
+//the for loop creates the equivalent of: <span id="0-0" class="tile">P</span> using JS means we don't have to write this out 30x. Also, if we decide to have a larger or smaller board, we just change the value of the "row" and "column" variables.
     for (let r = 0; r < height; r++) {
         for (let c = 0; c < width; c++) {
             //the below code is creating the tile elements: we start by creating a span, adding an id to the tile (like in excalidraw sketch), and then add the 'tile' id to that element which already has its styling specified. 
             let tile = document.createElement("span"); //span = like a new element but doesn't create new line so tiles can be placed next to each other. 
             tile.id = r.toString() + "-" + c.toString();
-            tile.classList("tile"); //adds the tile class with all its styling. 
+            tile.classList.add("tile"); //assigns the tile class (which is already in the css) to this element. 
+            tile.innerText = "P";
+            document.getElementById("board").appendChild(tile) //appends the tile to the "board" element created in our HTML. 
         }
     }
 }
