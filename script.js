c7fe1b7515d8c41b60b5623f93b713d3b41ece32
Asmahan <asmahanhmohamed@gmail.com>
2022-02-06 17:29:36 +0000

    The colour of each tile now changes when the word is entered

--- script.js
+++ script.js
@@ -11,10 +11,10 @@ window.onload = function(){
     initialize();
 }
 
-function initialize() {//will create tiles/game board and event listeners for game
+function initialize() {//will create tiles/game board and event listeners for game.
 
 //THE ACTUAL BOARD:
-//the following for loop should help us create 30 tile elements with  escalating ID numbers representing the current row (attempt) and column (current letter/position in the word)
+//the following for loop should help us create 30 tile elements with  escalating ID numbers representing the current row (attempt) and column (current letter/position in the word).
 //the for loop creates the equivalent of: <span id="0-0" class="tile">P</span> using JS means we don't have to write this out 30x. Also, if we decide to have a larger or smaller board, we just change the value of the "row" and "column" variables.
     for (let r = 0; r < height; r++) {
         for (let c = 0; c < width; c++) {
@@ -42,20 +42,20 @@ document.addEventListener("keyup", (e) => {
 }
 
 else if (e.code == "Backspace") {
-    if (0 < column && column <= width) { //if the tile we're on is in the 1st column and not beyond the 5th column (can't backspace on columns 0 and 6)ew
+    if (0 < column && column <= width) { //if the tile we're on is in the 1st column and not beyond the 5th column (can't backspace on columns 0 and 6).
         column -=1;
     }
     let currTile = document.getElementById(row.toString() + '-' +  column.toString());
     currTile.innerText = ""; 
 }
 
-else if  (e.code == "Enter") { //if user pushes enter key
-    update() //this method replaces the content of the element with the provided newContent argument and returns the element
-    row +=1 //move to the next row
-    column = 0 //start from the 1st tile on the new row
+else if  (e.code == "Enter") { //if user pushes enter key.
+    update() //this method replaces the content of the element with the provided newContent argument and returns the element.
+    row +=1 //move to the next row.
+    column = 0 //start from the 1st tile on the new row.
 }
 
-if (!gameOver && row == height) { //user has used up all their attempts
+if (!gameOver && row == height) { //user has used up all their attempts.
     gameOver = true;
     document.getElementById("answer").innerText=word //answer is revealed.
 }
@@ -69,21 +69,24 @@ function update() {
     let correct = 0; //iterating through each tile one by one to look at each letter. 
     for (let c = 0; c < width; c++) {
         let currTile = document.getElementById(row.toString() + "-" + c.toString());
-        let letter = currTile.innerText
+        let letter = currTile.innerText;
 
-        if (word[c] == letter) { //if the word at index c is equal to the letter the user entered...
-            currTile.classList.add("correct"); //...add "correct" to t hat tile's class list so that the styling can be applied. We're checking to see if each individual tile is correct.
-
-        else if (word.includes(letter)) {
-            currTile.classList.add("present"); //if the letter is only present, then the "present" classlist is added so that the styling can be applied. 
+        if (word[c] == letter) { //if word[c] is equal to the letter (the letter is not only present but is in the correct position)...
+            currTile.classList.add("correct"); //...add "correct" to that tile's class list so that the styling can be applied.
+            correct += 1;
         }
 
-        else { 
-                currTile.classList.add("absent"); //if the letter is not present in the word.
-    } 
+        else if (word.includes(letter)) {
+           currTile.classList.add("present"); //if the letter is only present, then the "present" classlist is added so that the styling can be applied. 
+        }
+
+       else { 
+           currTile.classList.add("absent"); //if the letter is not present in the word.
+   } 
     if (correct == width) {
         gameOver = true; //if we have 5 correct tiles, then it's game over.
     }
     }
 }
-}
+
+
